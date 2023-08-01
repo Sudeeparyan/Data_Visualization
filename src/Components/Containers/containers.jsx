89164d85a87decee9dc3649ceba5534eadd4ce3f
Manudev Rethinakumar <manudev.rethina@solitontech.local>
2023-08-01 12:57:41 +0530

    manudev_feat_uploadCsvButton_ExcelPage

---
+++ src/Components/Containers/containers.jsx
@@ -0,0 +1,12 @@
+import Excel from '../Pages/Excel/Excel'
+
+const Containers = () => {
+  return (
+    <>
+    <Excel/>
+    </>
+  )
+}
+
+export default Containers
+
---
+++ src/Components/Reusables/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Button.module.css'
+
+const ButtonComponent = () => {
+  return (
+    <div>
+       <button className={styles.viewButton}>Preview</button>
+    </div>
+  )
+}
+
+export default ButtonComponent
