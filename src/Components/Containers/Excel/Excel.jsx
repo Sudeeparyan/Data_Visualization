47fde9a96950f84116c0a4275d2c27375f458810
Manudev Rethinakumar <manudev.rethina@solitontech.local>
2023-08-03 14:32:47 +0530

    manudev_feat_addedCustomUrlRequest

--- src/Components/Containers/Excel/Excel.jsx
+++ src/Components/Containers/Excel/Excel.jsx
@@ -12,28 +12,38 @@ import { ExcelSelector } from "../../../Redux/ExcelPage/excelSelector";
 import { useSelector } from "react-redux";
 
 const Excel = () => {
+  //useSelector to get a specific Excel page data
   const Excel = useSelector(ExcelSelector.ExcelData);
-
   const navigate = useNavigate();
+
+  //POST Req RTK Query to send the uploaded csv
   const [sendExcelCSV] = useSendExcelCSVMutation() || {};
+  //GET Req to get the project from Backend
   const [getExcel, resultsExcel] = useLazyGetExcelQuery() || {};
 
+  //This Function is Trgiggered when a csv is selected by the user
   const handleCustomRequest = async ({ file, onError }) => {
     try {
       const formData = new FormData();
       formData.append("file", file);
+      //sending POST Request
       const res = await sendExcelCSV(formData);
+      //sending GET Request based on condition
       if (res.data.fileStatus === "success") await getExcel(res.data.projectId);
     } catch (error) {
-      onError(message.error("Error"));
+      onError(message.error("Error", error));
     }
   };
 
+  //Error Handling
   if (sendExcelCSV.isError) message.error("Error Uploading File!");
 
+  if (resultsExcel.data) console.log(resultsExcel.data);
+  //Redirecting the user to another Route when GET Req is Success
   if (resultsExcel.data) {
     navigate(`/Excel/${Excel.ProjectId}`);
   }
+
   return (
     <div>
       <div className={styles.heading}>
