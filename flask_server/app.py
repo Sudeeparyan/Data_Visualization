05e1387c13df8b3252b550dd67e9bd683c5d33b3
Giriharan <sushgh@yahoo.com>
2023-08-04 14:38:20 +0530

    Refact: bug fixes

--- flask_server/app.py
+++ flask_server/app.py
@@ -18,7 +18,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app)
 
 static_folder_path = os.path.abspath(os.path.join(os.getcwd(), '..', 'dist', 'assets'))
-app.static_folder = static_folder_path 
+app.static_folder = static_folder_path
 
 os.makedirs('storage\\database\\',exist_ok=True)
 db_file = os.path.join('storage', 'database', 'db.sqllite3')
@@ -34,23 +34,30 @@ with app.app_context():
     db.session.add(new_user)
     db.session.commit()
 
+file_pointer = open("../dist/index.html", "r", encoding="utf-8")
 
 @app.route("/")
 @cross_origin()
 @handle_errors
 def render_index_page():
     """Rendering the first page of the web page"""
-    file_pointer = open("../dist/index.html", "r", encoding="utf-8")
-
+    
     return file_pointer.read()
 
+
+
 @app.route("/assets/<path:name>")
+@cross_origin()
+@handle_errors
 def render_static_file(name):
     """Rendering  static files for the webpage"""
     return send_from_directory(
         app.static_folder, name, as_attachment=True
     )
+    
+
 
 if __name__ == '__main__':
     # Running the server at port 3000
     app.run(port=3000 ,debug=True)
+  
