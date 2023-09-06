d2d45b9ad1d96106315e5451c06da2481b937330
Giriharan <sushgh@yahoo.com>
2023-09-06 18:40:54 +0530

    Conflicts resolved

--- server/app.py
+++ server/app.py
@@ -19,36 +19,35 @@ from utilities import handle_errors
 
 # initializing the flask app
 app = create_app()
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.config["CORS_HEADERS"] = "Content-Type"
 cors = CORS(app)
 
 # configuring the static folder path for flask app
-static_folder_path = os.path.abspath(
-    os.path.join(os.getcwd(), '..', 'dist', 'assets'))
+static_folder_path = os.path.abspath(os.path.join(os.getcwd(), "..", "dist", "assets"))
 app.static_folder = static_folder_path
 
-
+@handle_errors("An error occured while initializing DB")
 def initialize_db():
     """Setting up sqllite3 connectivity"""
-    os.makedirs('storage\\database\\', exist_ok=True)
+    os.makedirs("storage\\database\\", exist_ok=True)
 
     # setting the file path for sqllite3
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-        os.path.join(os.getcwd(), 'storage\\database\\db.sqllite3')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+        os.getcwd(), "storage\\database\\db.sqllite3"
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
     # creating the db within flask context
     with app.app_context():
         db.create_all()
-        new_user = Users(
-            user_name='abc', email='abc@gmail.com', password='1234')
+        new_user = Users(user_name="abc", email="abc@gmail.com", password="1234")
         db.session.add(new_user)
         db.session.commit()
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
 @handle_errors("An error occured while reading html files")
 def index(path):
     """rendering the index page of the application"""
@@ -62,22 +61,19 @@ def index(path):
 def render_static_file(name):
     """Rendering  static files for the webpage"""
 
-    return send_from_directory(
-        app.static_folder, name, as_attachment=True
-    )
+    return send_from_directory(app.static_folder, name, as_attachment=True)
 
 
 @app.errorhandler(404)
 def handle_unknown_routes(error):
     """Function for handling unknown routes"""
 
-    response = jsonify(
-        {'error': 'The requested URL was not found on this server.'})
+    response = jsonify({"error": "The requested URL was not found on this server."})
     return response
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # setting up for db connectivity
     initialize_db()
     # Running the server at port 8000
-    app.run(port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
--- server/machine_learning/prediction_from_model.py
+++ server/machine_learning/prediction_from_model.py
@@ -11,7 +11,14 @@ import numpy as np
 from sklearn.preprocessing import PolynomialFeatures
 
 
-def generate_predictions(model_path: str, file_path: str, project_id: int) -> str:
+def generate_predictions(
+    model_path: str,
+    file_path: str,
+    project_id: int,
+    x_alias: str,
+    y_alias: str,
+    result_name: str,
+) -> str:
     """Using the best fit model to predict the actual data
 
     Args:
@@ -19,22 +26,24 @@ def generate_predictions(model_path: str, file_path: str, project_id: int) -> st
     file_path (str): relative path to the actual csv file
 
     Returns:
-    str: file path to the predicted csv and error csv file 
+    str: file path to the predicted csv and error csv file
     """
 
     actual_data = pd.read_csv(file_path)
     actual_data.dropna(inplace=True)
-    dependent_column = actual_data[actual_data.columns[0]]
-    independent_column = actual_data[actual_data.columns[1]]
+    dependent_column = actual_data[x_alias]
+    independent_column = actual_data[y_alias]
 
     # Load the polynomial model from pickle file
     with open(model_path, "rb") as fp:
         model_info = pickle.load(fp)
         model_name, best_degree, best_model, best_accuracy = model_info
+        
 
     # Generate points for the best fit polynomial curve
-    x_line = np.linspace(dependent_column.min(), dependent_column.max(
-    ), independent_column.shape[0]).reshape(-1, 1)
+    x_line = np.linspace(
+        dependent_column.min(), dependent_column.max(), independent_column.shape[0]
+    ).reshape(-1, 1)
 
     if model_name == "Polynomial Regression":
         poly = PolynomialFeatures(degree=best_degree)
@@ -46,27 +55,28 @@ def generate_predictions(model_path: str, file_path: str, project_id: int) -> st
     error = np.sqrt(np.square(y_line - independent_column))
 
     best_fit_coordinates = pd.DataFrame(
-        {'best_fit_X': x_line.flatten(), 'best_fit_Y': y_line})
+        {"best_fit_X": x_line.flatten(), "best_fit_Y": y_line}
+    )
     actual_coordinates = pd.DataFrame(
-        {actual_data.columns[0]: dependent_column, actual_data.columns[1]: independent_column})
-    error_coordinates = pd.DataFrame(
-        {'error_X': x_line.flatten(), 'error_Y': error})
+        {x_alias: dependent_column, y_alias: independent_column}
+    )
+    error_coordinates = pd.DataFrame({"error_X": x_line.flatten(), "error_Y": error})
 
     # Sort all DataFrames based on the same column
     result = pd.concat(
-        [best_fit_coordinates, error_coordinates, actual_coordinates], axis=1)
-    result = result.sort_values(by=[actual_data.columns[0]])
+        [best_fit_coordinates, error_coordinates, actual_coordinates], axis=1
+    )
+    result = result.sort_values(by=[x_alias])
 
     # file path for the result csv to be stored
     file_path = os.path.abspath(os.path.join(os.getcwd()))
-    file_path = os.path.join(file_path, "storage",
-                             "media_files", "project_files", str(project_id))
+    file_path = os.path.join(
+        file_path, "storage", "media_files", "project_files", str(project_id)
+    )
 
-    time_stamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     result.dropna(inplace=True)
 
-    result.to_csv(
-        file_path+f"//{project_id}_{time_stamp}_result.csv", index=False)
-    result_path = file_path+f"//{project_id}_{time_stamp}_result.csv"
+    result.to_csv(file_path + f"\\{result_name}.csv", index=False)
+    result_path = file_path + f"\\{result_name}.csv"
 
     return result_path
